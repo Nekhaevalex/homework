966cac90f5aa02887bab15bd96a7ee02c0fa3c0b
Alex <serg1221@macbook-pro-alex-2.local>
2016-09-26 11:13:22 +0300

    Initial Commit

---
+++ VSS Git/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  VSS Git
+//
+//  Created by Alex on 26/09/2016.
+//  Copyright © 2016 JL Computer Inc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
